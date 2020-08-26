aa3de80febf135bae7528a4ba0605f235237aaae
Latosha Tims <ltims080913@hotmail.com>
2020-08-26 16:10:43 -0700

    added alt text to HRClogo and created link to home

--- src/components/common/NavBar.js
+++ src/components/common/NavBar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import HRCpic from '../../assets/HRC(2).png';
 import '../../styles/index.css';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
   return (
@@ -8,15 +9,17 @@ const NavBar = () => {
       <nav className="nav-bar">
         <div className="HRClogo">
           <div className="logo-img-div">
-            <img src={HRCpic} className="logo-img" />
+            <Link to="/">
+              <img src={HRCpic} className="logo-img" alt="HRC logo" />
+            </Link>
           </div>
           <div className="logo-text-div">
-            <h3>
+            <h3 className="logo-text">
               Human <br /> Rights <br /> Considered
             </h3>
           </div>
         </div>
-        <a href="#">Sign Up</a>
+        <a href="/signup">Sign Up</a>
       </nav>
     </div>
   );
